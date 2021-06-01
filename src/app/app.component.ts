48c5f969f03cbcd44e2a73a79196c3939789e814
Hrvoje Bernardić <hrvoje.bernardic@mediatoolkit.com>
2021-06-01 20:53:04 +0200

    console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'github-actions-angular';
+
+  /**
+   *
+   */
+  constructor() {
+    console.log('hello ci');
+  }
 }
